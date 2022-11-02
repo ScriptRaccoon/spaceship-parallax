71a780ea355c7a1a30432db2d4a672ae528413b7
Script Raccoon <scriptraccoon@gmail.com>
2022-11-02 21:00:44 +0100

    convert canvas file typescript

--- src/ts/canvas.ts
+++ src/ts/canvas.ts
@@ -1,29 +1,46 @@
+export type canvasKey =
+	| "entityCanvas"
+	| "starCanvas1"
+	| "starCanvas2"
+	| "starCanvas3";
+
 export const canvas = {
-    entity: document.getElementById("entityCanvas"),
-    star1: document.getElementById("starCanvas1"),
-    star2: document.getElementById("starCanvas2"),
-    star3: document.getElementById("starCanvas3"),
+	entity: document.getElementById(
+		"entityCanvas"
+	) as HTMLCanvasElement,
+	star1: document.getElementById(
+		"starCanvas1"
+	) as HTMLCanvasElement,
+	star2: document.getElementById(
+		"starCanvas2"
+	) as HTMLCanvasElement,
+	star3: document.getElementById(
+		"starCanvas3"
+	) as HTMLCanvasElement,
 };
 
 export const ctx = {
-    entity: canvas.entity.getContext("2d"),
-    star1: canvas.star1.getContext("2d"),
-    star2: canvas.star2.getContext("2d"),
-    star3: canvas.star3.getContext("2d"),
+	entity: canvas.entity.getContext("2d"),
+	star1: canvas.star1.getContext("2d"),
+	star2: canvas.star2.getContext("2d"),
+	star3: canvas.star3.getContext("2d"),
 };
 
-function makeCanvasFullScreen(canv, factor = 1) {
-    canv.width = factor * window.innerWidth;
-    canv.height = factor * window.innerHeight;
+function makeCanvasFullScreen(
+	canvasEl: HTMLCanvasElement,
+	factor: number = 1
+) {
+	canvasEl.width = factor * window.innerWidth;
+	canvasEl.height = factor * window.innerHeight;
 }
 
 export function makeCanvasesFullScreen() {
-    makeCanvasFullScreen(canvas.entity);
-    makeCanvasFullScreen(canvas.star1, 2);
-    makeCanvasFullScreen(canvas.star2, 2);
-    makeCanvasFullScreen(canvas.star3, 2);
+	makeCanvasFullScreen(canvas.entity);
+	makeCanvasFullScreen(canvas.star1, 2);
+	makeCanvasFullScreen(canvas.star2, 2);
+	makeCanvasFullScreen(canvas.star3, 2);
 }
 
-export function clearCanvas(key) {
-    ctx[key].clearRect(0, 0, canvas[key].width, canvas[key].height);
+export function clearCanvas(key: canvasKey) {
+	ctx[key].clearRect(0, 0, canvas[key].width, canvas[key].height);
 }
