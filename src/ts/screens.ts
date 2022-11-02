b3744c90c82b2e08e65795b5fab3610ab9c21e1b
Script Raccoon <scriptraccoon@gmail.com>
2022-11-02 21:33:02 +0100

    convert screens file to typescript

--- src/ts/screens.ts
+++ src/ts/screens.ts
@@ -1,10 +1,19 @@
-const screens = document.querySelectorAll(".screen");
+type screenName = "loading" | "start" | "pause" | "gameover";
+
+const screens = {
+	loading: document.getElementById("loadingScreen"),
+	start: document.getElementById("startScreen"),
+	pause: document.getElementById("pauseScreen"),
+	gameover: document.getElementById("gameoverScreen"),
+} as Record<screenName, HTMLElement>;
 
 export function hideScreen() {
-    screens.forEach((screen) => screen.classList.remove("visible"));
+	for (const screen of Object.values(screens)) {
+		screen.classList.remove("visible");
+	}
 }
 
-export function showScreen(name) {
-    hideScreen();
-    document.getElementById(`${name}Screen`).classList.add("visible");
+export function showScreen(name: screenName) {
+	hideScreen();
+	screens[name].classList.add("visible");
 }
