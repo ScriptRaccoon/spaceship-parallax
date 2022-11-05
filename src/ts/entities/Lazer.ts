89b7d68256e67df9205c4af6da8240b16bdce735
Script Raccoon <scriptraccoon@gmail.com>
2022-11-05 13:13:15 +0100

    convert Lazer file to typescript

--- src/ts/entities/Lazer.ts
+++ src/ts/entities/Lazer.ts
@@ -1,66 +1,80 @@
 import { Asteroid } from "./Asteroid.js";
 import { canvas, ctx } from "../canvas.js";
-import { distance } from "../helper.js";
+import { coordinate, distance } from "../helper.js";
+import { type SpaceShip } from "./SpaceShip.js";
 
 export class Lazer {
-    static list = [];
+	static list: Lazer[] = [];
 
-    constructor({ pos, initialVel, rotation }) {
-        this.pos = pos;
-        this.rotation = rotation;
-        this.speed = 15;
-        this.vel = {
-            x: initialVel.x + this.speed * Math.cos(rotation),
-            y: initialVel.y + this.speed * Math.sin(rotation),
-        };
-        Lazer.list.push(this);
-    }
+	pos: coordinate;
+	rotation: number;
+	speed: number;
+	vel: coordinate;
 
-    draw() {
-        ctx.entity.save();
-        ctx.entity.fillStyle = "green";
-        ctx.entity.globalAlpha = 0.8;
-        ctx.entity.translate(this.pos.x, this.pos.y);
-        ctx.entity.rotate(this.rotation);
-        ctx.entity.fillRect(0, -2, 40, 4);
-        ctx.entity.fillStyle = "white";
-        ctx.entity.fillRect(0, -0.5, 40, 1);
-        ctx.entity.restore();
-    }
+	constructor({
+		pos,
+		initialVel,
+		rotation,
+	}: {
+		pos: coordinate;
+		initialVel: coordinate;
+		rotation: number;
+	}) {
+		this.pos = pos;
+		this.rotation = rotation;
+		this.speed = 15;
+		this.vel = {
+			x: initialVel.x + this.speed * Math.cos(rotation),
+			y: initialVel.y + this.speed * Math.sin(rotation),
+		};
+		Lazer.list.push(this);
+	}
 
-    update(ship) {
-        this.pos.x += this.vel.x;
-        this.pos.y += this.vel.y;
-        this.destroyAsteroids(ship);
-        this.removeIfOutside();
-    }
+	draw() {
+		ctx.entity.save();
+		ctx.entity.fillStyle = "green";
+		ctx.entity.globalAlpha = 0.8;
+		ctx.entity.translate(this.pos.x, this.pos.y);
+		ctx.entity.rotate(this.rotation);
+		ctx.entity.fillRect(0, -2, 40, 4);
+		ctx.entity.fillStyle = "white";
+		ctx.entity.fillRect(0, -0.5, 40, 1);
+		ctx.entity.restore();
+	}
 
-    removeIfOutside() {
-        if (
-            this.pos.x < 0 ||
-            this.pos.x > canvas.star1.width ||
-            this.pos.y < 0 ||
-            this.pos.y > canvas.star1.height
-        ) {
-            this.remove();
-        }
-    }
+	update(ship: SpaceShip) {
+		this.pos.x += this.vel.x;
+		this.pos.y += this.vel.y;
+		this.destroyAsteroids(ship);
+		this.removeIfOutside();
+	}
 
-    remove() {
-        Lazer.list = Lazer.list.filter((l) => l != this);
-    }
+	removeIfOutside() {
+		if (
+			this.pos.x < 0 ||
+			this.pos.x > canvas.star1.width ||
+			this.pos.y < 0 ||
+			this.pos.y > canvas.star1.height
+		) {
+			this.remove();
+		}
+	}
 
-    destroyAsteroids(ship) {
-        Asteroid.list.forEach((asteroid) => {
-            if (
-                distance(this.pos, asteroid.drawPos) <
-                asteroid.size / 2
-            ) {
-                asteroid.destroyed = true;
-                ship.score += asteroid.score;
-                ship.showScore();
-                this.remove();
-            }
-        });
-    }
+	remove() {
+		Lazer.list = Lazer.list.filter((l) => l != this);
+	}
+
+	destroyAsteroids(ship: SpaceShip) {
+		Asteroid.list.forEach((asteroid) => {
+			if (
+				distance(this.pos, asteroid.drawPos) <
+				asteroid.size / 2
+			) {
+				asteroid.destroyed = true;
+				ship.score += asteroid.score;
+				ship.showScore();
+				this.remove();
+			}
+		});
+	}
 }
